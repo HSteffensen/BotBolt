973f97717fc964056d0fe427766f3f0db7940f53
Henry Steffensen <henry@steffensenfamily.com>
2017-05-19 17:02:59 -0400

    initial 1

---
+++ bot.js
@@ -0,0 +1,18 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+const config = require("./config.json");
+
+client.login(config.token);
+
+client.on("ready", () => {
+  console.log("I am ready!");
+});
+
+client.on("message", (message) => {
+  if (message.author.bot) return; //ignore other bots
+  if (!message.content.startsWith(config.prefix)) return; //ignore any non-commands
+  let command = message.content.slice(1);
+  if (command.startsWith("ping")) {
+    message.channel.send("pong!");
+  }
+});
