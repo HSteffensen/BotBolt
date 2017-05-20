36f28dda5c6ba56adaea8838572c1ba6be233418
Henry Steffensen <henry@steffensenfamily.com>
2017-05-20 12:48:31 -0400

    Handle bad inputs better

--- commands/reload.js
+++ commands/reload.js
@@ -1,6 +1,12 @@
 exports.run = (client, message, args) => {
-  if(!args || args.size < 1) return message.channel.reply("Must provide a command name to reload.");
+  if(!args || args.length < 1) {
+    return message.reply("Bad request: must provide a command name to reload.");
+  }
   // the path is relative to the *current folder*, so just ./filename.js
-  delete require.cache[require.resolve(`./${args[0]}.js`)];
-  message.reply(`The command ${args[0]} has been reloaded`);
+  try {
+    delete require.cache[require.resolve(`./${args[0]}.js`)];
+  } catch(e) {
+    return message.reply("Bad request: command \"" + args.join(" ") + "\" not found.");
+  }
+  message.channel.send(`The command \"${args[0]}\" has been reloaded.`);
 };
