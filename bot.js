e5dd034d5fb0254db4efa36ae5a52b1f6f191505
Henry Steffensen <henry@steffensenfamily.com>
2017-05-21 15:08:36 -0400

    multiple commands in one line

--- bot.js
+++ bot.js
@@ -12,21 +12,31 @@ client.on("ready", () => {
 
 client.on("message", (message) => {
   if (message.author.bot) return; //ignore other bots
-  if (!message.content.startsWith(config.prefix)) {
-    return; //ignore any non-commands for now
-  } else {
-    let messageSplit = message.content.split(" ");
-    let command = messageSplit[0].slice(config.prefix.length);
-    let args = messageSplit.slice(1);
+  let commands = parseMessageForCommands(message);
+  for(let i = 0; i < commands.length; i++) {
+    let line = commands[i].trim();
+    if (!line.startsWith(config.prefix)) {
+      return; //ignore any non-commands for now
+    } else {
+      let messageSplit = line.split(" ");
+      let command = messageSplit[0].slice(config.prefix.length);
+      let args = messageSplit.slice(1);
 
-    let commandSanitize = /\b\w+\b/; //test for anything other than [a-z], [A-Z], [0-9], or '_'. reject if found.
-    if(commandSanitize.test(command)) {
-      try {
-        let commandFile = require(`./commands/${command}.js`);
-        commandFile.run(client, message, args, config, sql);
-      } catch (err) {
-        console.error(err);
+      let commandSanitize = /\b\w+\b/; //test for anything other than [a-z], [A-Z], [0-9], or '_'. reject if found.
+      if(commandSanitize.test(command)) {
+        try {
+          let commandFile = require(`./commands/${command}.js`);
+          commandFile.run(client, message, args, config, sql);
+        } catch (err) {
+          console.error(err);
+        }
       }
     }
   }
 });
+
+function parseMessageForCommands(message) {
+  let commandRegexp = /([^\{\}\;]+\{[^\{\}]+\}|[^\{\}\;]+)/g; //splits commands by semicolon or allows semicolons inside {}
+  let commandSplit = message.content.match(commandRegexp);
+  return commandSplit;
+}
