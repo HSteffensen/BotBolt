a491c7b7921a95cef7854f3053c69d1f1adf0a6e
Henry Steffensen <henry@steffensenfamily.com>
2017-05-21 01:05:47 -0400

    money accepts multiple mentions

--- commands/money.js
+++ commands/money.js
@@ -1,31 +1,30 @@
 exports.run = (client, message, args, config, sql) => {
   // Decide which user to check. Either caller or the mentioned name.
-  let user = (!args || args.length < 1) ? message.author : message.mentions.users.first();
-  let userID = user.id;
-  /*
-  if(!args || args.length < 1) {
-    userID = message.author.id;
-  } else {
-    userID = message.mentions.users.first().id;
-  }
-  */
-
-  let amount = 0;
-  sql.get(`SELECT * FROM money WHERE userID ="${userID}"`).then(row => {
-    if(!row) {
-      sql.run("INSERT INTO money (userID, amount) VALUES (?, ?)", [userID, 0]);
-    } else {
-      amount = row.amount;
-    }
-  }).catch(() => {
-    console.error;
-    sql.run("CREATE TABLE IF NOT EXISTS money (userId TEXT, amount INTEGER)").then(() => {
-      sql.run("INSERT INTO money (userId, amount) VALUES (?, ?)", [userID, 0]);
+  let users = (message.mentions.users.array().length > 0) ? message.mentions.users.array() : [message.author];
+  console.log(message.mentions);
+  let description = "";
+  for(let i = 0, len = users.length; i < len; i++) {
+    let user = users[i];
+    let amount = 0;
+    let userID = user.id;
+    sql.get(`SELECT * FROM money WHERE userID ="${userID}"`).then(row => {
+      if(!row) {
+        sql.run("INSERT INTO money (userID, amount) VALUES (?, ?)", [userID, 0]);
+      } else {
+        amount = row.amount;
+      }
+    }).catch(() => {
+      console.error;
+      sql.run("CREATE TABLE IF NOT EXISTS money (userId TEXT, amount INTEGER)").then(() => {
+        sql.run("INSERT INTO money (userId, amount) VALUES (?, ?)", [userID, 0]);
+      });
     });
-  });
+    description += `**${user.tag}** has \$${amount}.\n`;
+  }
+
 
   message.channel.send("", {embed: {
     color: config.color,
-    description: `**${user.tag}** has \$${amount}.`
+    description: `${description}`
   }});
 };
