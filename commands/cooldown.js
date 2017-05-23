0d3436003a336ad568f993f9d718f8036cc053ba
Henry Steffensen <henry@steffensenfamily.com>
2017-05-22 23:09:20 -0400

    cooldown verbosity and punishment setup

--- commands/cooldown.js
+++ commands/cooldown.js
@@ -35,12 +35,36 @@ exports.run = async (client, message, command, config, sql, shortcut) => {
   }
 
   let numberCheck = /\b\d+\b/;
-  if(!numberCheck.test(args[1])) {
-    return message.reply("Bad request: must provide number in seconds, 0 to remove.");
+
+  if(args[1] === "remove") {
+    return await removeCooldown(client, message, command, config, sql, commandName);
   }
 
-  if(args[1] === "0") {
-    return await removeCooldown(client, message, command, config, sql, commandName);
+  if(args[1] === "verbosity") {
+    let verbosityCheck = /\b(0|1|2|off|on|default|enable|disable)\b/;
+    if(!verbosityCheck.test(args[2])) {
+      return message.reply("verbosity requires 0, 1, or 2.");
+    }
+    let verbosityMap = {
+      "off": "0",
+      "on": "1",
+      "disable": "0",
+      "enable": "1",
+      "default": "2"
+    };
+    let input = (verbosityMap[args[2]]) ? verbosityMap[args[2]] : args[2];
+    return await setVerbosity(client, message, command, config, sql, commandName, input);
+  }
+
+  if(args[1] === "punishment" || args[1] === "punish") {
+    if(!numberCheck.test(args[2])) {
+      return message.reply("punishment requires an integer.");
+    }
+    return await setPunishment(client, message, command, config, sql, commandName);
+  }
+
+  if(!numberCheck.test(args[1])) {
+    return message.reply("Bad request: must provide number in seconds, or one of ` remove | verbosity | punishment `.");
   }
 
   await setCooldown(client, message, command, config, sql, commandName);
@@ -48,23 +72,33 @@ exports.run = async (client, message, command, config, sql, shortcut) => {
 
 async function checkCooldown(client, message, command, config, sql, commandName) {
   let time = 0;
+  let verbosity = 0;
+  let punishment = 0;
   let description = "";
+  let exists = false;
 
   try {
     let row = await sql.get(`SELECT * FROM cooldowns WHERE commandName ="${commandName}"`);
     if(row) {
       time = row.downtime;
+      verbosity = row.verbosity;
+      punishment = row.punishment;
+      exists = true;
     }
   } catch(e) {
     console.error(e);
     console.log("Creating table cooldowns");
-    await sql.run("CREATE TABLE IF NOT EXISTS cooldowns (commandName TEXT, downtime INTEGER)");
+    await sql.run("CREATE TABLE IF NOT EXISTS cooldowns (commandName TEXT, downtime INTEGER, verbosity INTEGER, punishment INTEGER)");
   }
 
-  if(time == 0) {
-    description = `**!${commandName}** has no cooldown.`
+  let verbosityOutput = ["is disabled", "is enabled", "defaults to global setting"];
+
+  if(!exists) {
+    description = `**!${commandName}** has no cooldown configuration.`;
   } else {
-    description = `Cooldown for **!${commandName}** is ${time} seconds.`;
+    description += `Cooldown for **!${commandName}** is ${time} seconds.\n`;
+    description += `Time left alert ${verbosityOutput[verbosity]}.\n`;
+    description += `Punishment is ${punishment} added seconds.\n`;
   }
 
   message.channel.send("", {embed: {
@@ -80,15 +114,15 @@ async function setCooldown(client, message, command, config, sql, commandName) {
   try {
     let row = await sql.get(`SELECT * FROM cooldowns WHERE commandName ="${commandName}"`);
     if(!row) {
-      await sql.run("INSERT INTO cooldowns (commandName, downtime) VALUES (?, ?)", [commandName, time]);
+      await sql.run("INSERT INTO cooldowns (commandName, downtime, verbosity, punishment) VALUES (?, ?, ?, ?)", [commandName, time, 2, 0]);
     } else {
-      await sql.run("UPDATE cooldowns SET downtime = ? WHERE commandName = ", [time, commandName]);
+      await sql.run("UPDATE cooldowns SET downtime = ? WHERE commandName = ?", [time, commandName]);
     }
   } catch(e) {
     console.error(e);
     console.log("Creating table cooldowns");
-    await sql.run("CREATE TABLE IF NOT EXISTS cooldowns (commandName TEXT, downtime INTEGER)");
-    await sql.run("INSERT INTO cooldowns (commandName, downtime) VALUES (?, ?)", [commandName, time]);
+    await sql.run("CREATE TABLE IF NOT EXISTS cooldowns (commandName TEXT, downtime INTEGER, verbosity INTEGER, punishment INTEGER)");
+    await sql.run("INSERT INTO cooldowns (commandName, downtime, verbosity, punishment) VALUES (?, ?, ?, ?)", [commandName, time, 2, 0]);
   }
 
   message.channel.send("", {embed: {
@@ -98,29 +132,76 @@ async function setCooldown(client, message, command, config, sql, commandName) {
 }
 
 async function removeCooldown(client, message, command, config, sql, commandName) {
-  let time = 0;
+  let exists = false;
   let description = "";
+
 
   try {
     let row = await sql.get(`SELECT * FROM cooldowns WHERE commandName ="${commandName}"`);
     if(row) {
-      time = row.downtime;
+      exists = true;
       await sql.run("DELETE FROM cooldowns WHERE commandName = ?", [commandName]);
     }
   } catch(e) {
     console.error(e);
     console.log("Creating table cooldowns");
-    await sql.run("CREATE TABLE IF NOT EXISTS cooldowns (commandName TEXT, downtime INTEGER)");
+    await sql.run("CREATE TABLE IF NOT EXISTS cooldowns (commandName TEXT, downtime INTEGER, verbosity INTEGER, punishment INTEGER)");
   }
 
-  if(time == 0) {
-    description = `**!${commandName}** has no cooldown.`
+  if(!exists) {
+    description = `**!${commandName}** has no cooldown configuration.`;
   } else {
-    description = `Cooldown removed for **!${commandName}**.`
+    description = `Cooldown configuration removed for **!${commandName}**.`;
   }
 
   message.channel.send("", {embed: {
     color: config.color,
     description: description
+  }});
+}
+
+async function setVerbosity(client, message, command, config, sql, commandName, verbosity) {
+  try {
+    let row = await sql.get(`SELECT * FROM cooldowns WHERE commandName ="${commandName}"`);
+    if(!row) {
+      await sql.run("INSERT INTO cooldowns (commandName, downtime, verbosity, punishment) VALUES (?, ?, ?, ?)", [commandName, 0, verbosity, 0]);
+    } else {
+      await sql.run("UPDATE cooldowns SET verbosity = ? WHERE commandName = ?", [verbosity, commandName]);
+    }
+  } catch(e) {
+    console.error(e);
+    console.log("Creating table cooldowns");
+    await sql.run("CREATE TABLE IF NOT EXISTS cooldowns (commandName TEXT, downtime INTEGER, verbosity INTEGER, punishment INTEGER)");
+    await sql.run("INSERT INTO cooldowns (commandName, downtime, verbosity, punishment) VALUES (?, ?, ?, ?)", [commandName, 0, verbosity, 0]);
+  }
+
+  let verbosityOutput = ["disabled", "enabled", "default to global setting"];
+
+  message.channel.send("", {embed: {
+    color: config.color,
+    description: `Verbosity of **!${commandName}** cooldown set to ${verbosityOutput[verbosity]}.`
+  }});
+}
+
+async function setPunishment(client, message, command, config, sql, commandName) {
+  let punishment = command.args[2];
+
+  try {
+    let row = await sql.get(`SELECT * FROM cooldowns WHERE commandName ="${commandName}"`);
+    if(!row) {
+      await sql.run("INSERT INTO cooldowns (commandName, downtime, verbosity, punishment) VALUES (?, ?, ?, ?)", [commandName, 0, 2, punishment]);
+    } else {
+      await sql.run("UPDATE cooldowns SET punishment = ? WHERE commandName = ?", [punishment, commandName]);
+    }
+  } catch(e) {
+    console.error(e);
+    console.log("Creating table cooldowns");
+    await sql.run("CREATE TABLE IF NOT EXISTS cooldowns (commandName TEXT, downtime INTEGER, verbosity INTEGER, punishment INTEGER)");
+    await sql.run("INSERT INTO cooldowns (commandName, downtime, verbosity, punishment) VALUES (?, ?, ?, ?)", [commandName, 0, 2, punishment]);
+  }
+
+  message.channel.send("", {embed: {
+    color: config.color,
+    description: `Punishment for calling **!${commandName}** during cooldown set to ${punishment} added seconds.`
   }});
 }
