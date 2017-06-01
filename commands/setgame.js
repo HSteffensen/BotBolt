0c49f0135c0fb071eaf30ab044dfcea3f15f8d56
Henry Steffensen <henry@steffensenfamily.com>
2017-05-31 20:11:32 -0400

    help initial

--- commands/setgame.js
+++ commands/setgame.js
@@ -1,5 +1,17 @@
 exports.run = (client, message, command, config) => {
   let args = command.args;
+  // Caller must be bot owner
+  let permitted = false;
+  for(let i = 0, len = config.ownerIDs.length; i < len; i++) {
+    let owner = config.ownerIDs[i];
+    if(owner == message.author.id) {
+      permitted = true;
+      break;
+    }
+  }
+  if(!permitted) {
+    return message.reply("Permission denied: setgame");
+  }
   let gameStr = args.join(" ");
   client.user.setGame(gameStr);
   if(!command.silent) {
